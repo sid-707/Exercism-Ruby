ad5f8aa7d6708958c15de81638eb9124491cda59
sid-707 <95631365+sid-707@users.noreply.github.com>
2024-06-30 07:56:00 -0400

    bird-count

---
+++ bird-count/bird_count.rb
@@ -0,0 +1,27 @@
+class BirdCount
+  @birds_per_day
+
+  def self.last_week
+    [0, 2, 5, 3, 7, 8, 4]
+  end
+
+  def initialize(birds_per_day)
+    @birds_per_day = birds_per_day
+  end
+
+  def yesterday
+    @birds_per_day[-2]
+  end
+
+  def total
+    @birds_per_day.sum
+  end
+
+  def busy_days
+    @birds_per_day.count { |number| number >= 5 }
+  end
+
+  def day_without_birds?
+    @birds_per_day.any?   { |number| number == 0 }
+  end
+end
